905c48af39245bd185ef1f9e9829860f7375fb67
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2023-04-30 11:27:47 +0600

    feat: basic collector added to listen user create event

--- collector/app.py
+++ collector/app.py
@@ -1,1 +1,41 @@
-print("Data Collector")
+import pika
+import pymongo
+import json
+
+# Database connection
+client = pymongo.MongoClient("mongodb://admin:password@mongodb2:8012/smsystem")
+db = client["smsystem"]
+
+# RabbitMQ connection
+credentials = pika.PlainCredentials('guest', 'guest')
+parameters = pika.ConnectionParameters('rabbitmq2', 5672, '/', credentials)
+
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+channel.queue_declare(queue='backup-data', durable=False)
+
+'''
+    function for consuming message from rabbitmq
+'''
+
+
+def callback(ch, method, properties, body):
+    print("Received message:", body.decode())
+
+    data = json.loads(body.decode())
+
+    print(data)
+
+    if (data['type'] == "user"):
+        if (data['action'] == "create"):
+            db.users.insert_one(data['data'])
+
+    db.message.insert_one({"message": data})
+
+
+channel.basic_consume(
+    queue='backup-data', on_message_callback=callback, auto_ack=True)
+
+print('Listening to queue "test". Press Ctrl+C to stop.')
+channel.start_consuming()
